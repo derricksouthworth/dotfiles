b0831c85b21bde542b7bbaaca5526736f21ddd7d
derricksouthworth <derricksouthworth@gmail.com>
2025-01-15 16:46:54 -0700

    Add local hyprland config to dotfiles

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-#                    __           
-#  _    _____ ___ __/ /  ___ _____
-# | |/|/ / _ `/ // / _ \/ _ `/ __/
-# |__,__/\_,_/\_, /_.__/\_,_/_/   
-#            /___/                
-# 
-# ----------------------------------------------------- 
-# Quit all running waybar instances
-# ----------------------------------------------------- 
-killall waybar
-pkill waybar
-sleep 0.5
 
-# ----------------------------------------------------- 
-# Start new waybar instance
-# ----------------------------------------------------- 
-waybar &
+# Kill running instances
+killall -q waybar
+
+# Wait for running processes
+while pgrep -x waybar >/dev/null; do
+    sleep 0.1;
+done
+
+# Launch bar
+waybar -c ~/.config/waybar/config.jsonc & -s ~/.config/waybar/style.css
